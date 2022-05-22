bdb246fbd809018a1ff0e08546cfdfa59f5654b6
Raphael Gomes Prazeres <rafagomes1706@gmail.com>
2022-05-22 15:57:45 -0300

    adciona icons, tranfere para pagina home

--- Pages/login/login.js
+++ Pages/login/login.js
@@ -1,38 +1,92 @@
 const button = document.getElementById('btn-submit')
 
 button.addEventListener('click', (evento) => {
-    evento.preventDefault()
-
-    const email = document.getElementById('email')
-    const senha = document.getElementById('senha')
-
-    //validar email//
-
-    if(email.value == ''){
-        email.classList.add("errorInput")   //adciona class ao input//
-    }
-
-    else if(email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1 || (email.value.indexOf(".") - email.value.indexOf("@") == 1)){ //verifica se possue @, . e se a distancia entre eles é maior que 1//
-        email.classList.add("errorInput")
     
-    }else{
-        email.classList.remove("errorInput")
-        email.classList.add("okInput")
-    }
-
-    //validar senha//
-
-    if(senha.value == ''){
-        senha.classList.add("errorInput")
-    
-    }else if(senha.value.length < 6 ){   //senha menor que 6 digitos//
-        senha.classList.add("errorInput")
-
-    }else{
-        senha.classList.remove("errorInput")
-        senha.classList.add("okInput")
-    }    
-
+    evento.preventDefault();
+    validarEmail();
+    validarSenha();
+    validacao();
 })
 
-//irei por mensagem para cada erro, tentar fazer com switch case e recaregar a pagina/ir para outra caso os campos estejam corretos//
+function validarEmail(){
+    
+    const email = document.getElementById('email');
+    const emailErro = document.getElementById('erro-email');        //mensagem de erro//
+    const padrao =  /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+        //validacao do email//
+
+    if(email.value.match(padrao)){
+
+        email.classList.add('valid');
+        email.classList.remove('invalid');
+        emailErro.innerHTML = "Seu email é válido!"
+        emailErro.style.color = "#00ff00";
+        $("#valid").css("visibility", "visible");
+        $("#invalid").css("visibility", "hidden");
+        return true;
+
+    }else if(email.value == ""){
+        email.classList.remove('valid');
+        email.classList.add('invalid');
+        emailErro.innerHTML = "Esse campo deve ser preenchido."
+        emailErro.style.color = "#ff0000";
+        $("#invalid").css("visibility", "visible");
+        $("#valid").css("visibility", "hidden");
+        return false;
+    
+    }else{
+        email.classList.remove('valid');
+        email.classList.add('invalid');
+        emailErro.innerHTML = "Por favor, insira um email válido!"
+        emailErro.style.color = "#ff0000";
+        $("#invalid").css("visibility", "visible");
+        $("#valid").css("visibility", "hidden");
+        return false;
+    }
+}
+        //validacao da senha//
+
+    function validarSenha(){   
+
+    const senha = document.getElementById('senha');
+    const senhaErro = document.getElementById('erro-senha'); 
+
+    if (senha.value.length >= 6 && senha.value.length <= 12){
+
+        senha.classList.add('valid');
+        senha.classList.remove('invalid');
+        senhaErro.innerHTML = "Sua senha é válida!"
+        senhaErro.style.color = "#00ff00";
+        $("#invalid1").css("visibility", "hidden");
+        $("#valid1").css("visibility", "visible");
+        return true;
+
+    }else if(senha.value == ""){
+        senha.classList.remove('valid');
+        senha.classList.add('invalid');
+        senhaErro.innerHTML = "Esse campo deve ser preenchido."
+        senhaErro.style.color = "#ff0000";
+        $("#invalid1").css("visibility", "visible");
+        $("#valid1").css("visibility", "hidden");
+        return false;
+    
+    }else{
+        senha.classList.remove('valid');
+        senha.classList.add('invalid');
+        senhaErro.innerHTML = "Por favor, insira uma senha válido!"
+        senhaErro.style.color = "#ff0000";
+        $("#invalid1").css("visibility", "visible");
+        $("#valid1").css("visibility", "hidden");
+        return false;
+    }
+}
+
+//mudar pagina quando os valores estiverem certos//
+
+function validacao(){
+    
+    if(validarEmail() == true && validarSenha() == true){
+        return window.location.href = "../../index.html"
+    }
+}
--- Pages/recuperaSenha/RecuperarSenha.js
+++ Pages/recuperaSenha/RecuperarSenha.js
@@ -1,21 +1,45 @@
 const button = document.getElementById('btn-recuperacaoSenha')
 
 button.addEventListener('click', (evento) => {
+    
     evento.preventDefault()
-
-    const email = document.getElementById('email')
-    
-    
-        if(email.value == ''){
-            email.classList.add("errorInput")  //adciona class ao input//
-        }
-
-        else if(email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1 || (email.value.indexOf(".") - email.value.indexOf("@") == 1)){    //verifica se possue @, . e se a distancia entre eles é maior que 1//
-            email.classList.add("errorInput")
-        
-        }else{
-            email.classList.remove("errorInput")
-            email.classList.add("okInput")
-        }
-
+    validar();
 })
+
+function validar(){
+    
+    const email = document.getElementById('email');
+    const emailErro = document.getElementById('erro-email');
+    const padrao =  /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if(email.value.match(padrao)){
+
+        email.classList.add('valid');
+        email.classList.remove('invalid');
+        emailErro.innerHTML = "Seu email é válido!"
+        emailErro.style.color = "#00ff00";
+        $("#valid").css("visibility", "visible");
+        $("#invalid").css("visibility", "hidden");
+
+    }else if(email.value == ""){
+
+        email.classList.remove('valid');
+        email.classList.add('invalid');
+        emailErro.innerHTML = "Esse campo deve ser preenchido."
+        emailErro.style.color = "#ff0000";
+        $("#valid").css("visibility", "hidden");
+        $("#invalid").css("visibility", "visible");
+    
+    }else{
+
+        email.classList.remove('valid');
+        email.classList.add('invalid');
+        emailErro.innerHTML = "Por favor, insira um email válido!"
+        emailErro.style.color = "#ff0000";
+        $("#valid").css("visibility", "hidden");
+        $("#invalid").css("visibility", "visible");
+    }
+
+
+}
+
